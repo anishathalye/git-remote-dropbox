anishathalye/git-remote-dropbox

commit c985a0aa335d636e4ccfbf907b37392e912d5a44

Add workaround for Dropbox SDK dependency issue
The Dropbox PyPI package does not specify the correct requests version
as a dependency. This patch works around the issue by raising the
minimum requests version required. See
https://github.com/dropbox/dropbox-sdk-python/issues/114 for more
details.

setup.py
@@ -65,7 +65,10 @@ setup(
 
     packages=['git_remote_dropbox'],
 
-    install_requires=['dropbox>=8.0,<9'],
+    install_requires=[
+        'dropbox>=8.0,<9',
+        'requests>=2.18.1'
+    ],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow

commit 76d29c43d529e8713309d9cb65e1d264030fd165

Remove workaround for Dropbox SDK dependency issue
Commit c985a0aa335d636e4ccfbf907b37392e912d5a44 introduced a workaround
for a dependency issue with the Dropbox SDK for Python. This issue has
since been resolved, so we can remove our workaround and only depend
directly on the Dropbox PyPI package.

setup.py
@@ -66,8 +66,7 @@ setup(
     packages=['git_remote_dropbox'],
 
     install_requires=[
-        'dropbox>=8.0,<9',
-        'requests>=2.18.1'
+        'dropbox>=8.5,<9',
     ],
 
     # To provide executable scripts, use entry points in preference to the